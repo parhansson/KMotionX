6534e69df543b28d9595b845c92969e114d95333
Pär Hansson <par.hansson@gmail.com>
2015-04-25 00:58:05 +0200

    Settings for laser cutter.

--- KMotionX/examples/webgui/site/c-programs/LaserPPI.c
+++ KMotionX/examples/webgui/site/c-programs/LaserPPI.c
@@ -3,24 +3,30 @@
 #define CLOCK 16.6666e6
 #define ON 1
 #define OFF 0
+#define PULSE_LENGTH 2 // was and should be 2ish
+#define PPI_IO_BIT 14
+
 //http://www.buildlog.net/blog/2011/12/getting-more-power-and-cutting-accuracy-out-of-your-home-built-laser-system/
 main() {
   //M100 P500 Q50
   double ppi = *(float *) &persist.UserData[0]; //1500;//8000; //mm per min P Parameter
-  ppi = ppi * 2; // double to just to make it work.
-
-
   double percent = *(float *) &persist.UserData[1]; //50%; //Q parameter
+  printf("LaserPPI.c M100 P = %f Q = %f\n", ppi, percent);
+  
   double dutycycle = 255 * ((100 - percent) / 100);
+  printf("LaserPPI.c adjusted PPI = %f DutyCycle = %f\n", ppi, dutycycle);
+
+
   SetBitDirection(26, 1);    // Set bit 26 (PWM 0 as an output)
   FPGA(IO_PWMS_PRESCALE) = 2;   // set pwm frequency 21.7KHz KHz
   FPGA(IO_PWMS+1) = 1;   // enable the PWM0
   FPGA(IO_PWMS+0) = dutycycle;   //12;//4.7%  // Set duty cycle to 25%
 
-  printf("P = %f Q = %f\n", ppi, dutycycle);
+  
+  ppi = ppi * 2; // double to just to make it work for some reason.
 
-  SetBitDirection(14, 1);
-  ClearBit(14);
+  SetBitDirection(PPI_IO_BIT, 1);
+  ClearBit(PPI_IO_BIT);
   //SetBitDirection(4, 1);
   /*
    https://groups.yahoo.com/neo/groups/DynoMotion/conversations/messages/8717
@@ -49,7 +55,7 @@ main() {
    Hur manga timebase ar det
    (1/TIME_BASE/cf)
    */
-  double pulseLength = 2; //ms
+  double pulseLength = PULSE_LENGTH; //ms
   int pulseLengthPeriods = (int) (pulseLength / 1000 / TIMEBASE) / 2; // divide by two for some strange reason
   //printf("Pulse length=%fms, Periods=%d\n",pulseLentgh, pulseLengthPeriods);
   //ppi = 500;
@@ -62,12 +68,13 @@ main() {
   double T;
   double LastPrintT = Time_sec();
   int laserState = OFF;
+  SetStateBit(PPI_IO_BIT, laserState);
   int periods = 0, lastPeriod = 0, onPeriods = 0;
   for (;;) {
 
     //Delay_sec(0.1);
     WaitNextTimeSlice();  // ensure we don't get interrupted
-    if (laserState == 1) {
+    if (laserState == ON) {
       onPeriods++;
     }
     periods++;
@@ -79,9 +86,12 @@ main() {
     //step
     if (speed < 0.9) {
       //head is not moving turn laser off
-      laserState = OFF;
+      if(laserState == ON){
+        laserState = OFF;
+        SetStateBit(PPI_IO_BIT, laserState);
+        printf("Movement Stopped %d\n", laserState);
+      }
       onPeriods = 0;
-      SetStateBit(14, laserState);
       lastPeriod = periods = 0;
 
     } else if (laserState == ON) {
@@ -90,7 +100,8 @@ main() {
       if (onPeriods > pulseLengthPeriods) {
         onPeriods = 0;
         laserState = OFF;
-        SetStateBit(14, laserState);
+        SetStateBit(PPI_IO_BIT, laserState);
+        //printf("LaserState %d\n", laserState);
       }
     } else {
       // head is moving an laser is off
@@ -103,19 +114,19 @@ main() {
 
         laserState = ON;
         onPeriods = 0;
-        SetStateBit(14, laserState);
-
+        SetStateBit(PPI_IO_BIT, laserState);
+        //printf("LaserState %d\n", laserState);
         if (periods > 2000000000) periods = 0;
         lastPeriod = periods;
       }
     }
 
-    /*
+
      T=Time_sec();
      if(T-LastPrintT > 0.5){
-     printf("Speed=%f steps/sec, %fmm/s pps=%f\n",speed, mmps,pps);
-     LastPrintT = T;
+       printf("Speed=%f steps/sec, %fmm/s pps=%f\n",speed, mmps,pps);
+       LastPrintT = T;
      }
-     */
+
   }
 }
