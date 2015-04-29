c39e6c3d313998373ac92b34ccd98bb58d683cff
Pär Hansson <par.hansson@gmail.com>
2015-04-29 22:03:55 +0200

    Added intermediate machine object on load.
    Use angular.copy to mirror fetched object.
    This helps with some strange error in angular. I have not found the
    actual cause but this makes the error less frequent.

--- KMotionX/examples/webgui/site/js/settings/settings.service.js
+++ KMotionX/examples/webgui/site/js/settings/settings.service.js
@@ -8,7 +8,18 @@
     var mcodes = ['','','','M3', 'M4', 'M5', 'M6', 'M7', 'M8', 'M9', 'S'];
     var mcodesExtended = ["M100", "M101", "M102", "M103", "M104", "M105", "M106", "M107", "M108", "M109", "M110", "M111", "M112", "M113", "M114", "M115", "M116", "M117", "M118", "M119"];
     
-    var data = {
+    var data = newMachine();
+    
+    var service =  {
+      machine: data,
+      load: load,
+      save: save,
+      fileName: fileName
+    };
+    return service;
+    
+    function newMachine(){
+      return {
         name: null,
         description: null,
         dimX: 0,
@@ -21,47 +32,39 @@
         actions: actionsArr(mcodes),
         extendedActions: actionsArr(mcodesExtended),
         userActions: userActionsArr()
-    };
-
-    var service =  {
-      machine:data,
-      load: load,
-      save: save,
-      fileName: fileName
-    };
-    return service;
+      };
+    }
     
     function load(file) {
-      return $http.get(file).success(function(jsonData) {
+      return $http.get(file +"?nocache=1").success(function(jsonData) {
         updateSettings(jsonData);
       });
     }
       
     function updateSettings(jsonData) {
-      data.name = jsonData.name;
-      data.description = jsonData.description;
-      data.dimX = jsonData.dimX;
-      data.dimY = jsonData.dimY;
-      data.dimZ = jsonData.dimZ;
-      data.initProgram = jsonData.initProgram;
-      data.initThread = jsonData.initThread;
-      data.axes = jsonData.axes || axesArr();
-      data.tplanner = jsonData.tplanner || {};
-      data.actions = jsonData.actions || actionsArr(mcodes);
-      data.extendedActions = jsonData.extendedActions || actionsArr(mcodesExtended);
-      data.userActions = jsonData.userActions || userActionsArr();
+      var machineData = newMachine();
+      machineData.name = jsonData.name;
+      machineData.description = jsonData.description;
+      machineData.dimX = jsonData.dimX;
+      machineData.dimY = jsonData.dimY;
+      machineData.dimZ = jsonData.dimZ;
+      machineData.initProgram = jsonData.initProgram;
+      machineData.initThread = jsonData.initThread;
+      machineData.axes = jsonData.axes || axesArr();
+      machineData.tplanner = jsonData.tplanner || {};
+      machineData.actions = jsonData.actions || actionsArr(mcodes);
+      machineData.extendedActions = jsonData.extendedActions || actionsArr(mcodesExtended);
+      machineData.userActions = jsonData.userActions || userActionsArr();
+      angular.copy(machineData, data);
     }
     
     function fileName(){
-      return "settings/" +data.name + ".cnf";
+      return "settings/" +service.machine.name + ".cnf";
     }
     
     function save() {
       var file = fileName(); 
-      var param = {};
-      var copy = angular.copy(data);
-      param[file] = copy;
-      kmxBackend.save(file, angular.toJson(data,true));
+      kmxBackend.save(file, angular.toJson(service.machine,true));
     }    
     function axesArr(){
       var axes = [] 
