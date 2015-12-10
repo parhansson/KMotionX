f4b4217445eecfa3aaa1c06953069d179efda306
Pär Hansson <par.hansson@gmail.com>
2015-12-10 16:38:13 +0100

    Deleted build files

--- KMotionX/KMotionXCNC/server/build/utils.d
+++
@@ -1,8 +0,0 @@
-build/utils.o: utils.cpp utils.h mongoose.h \
-  /Users/parhansson/git/KMotionX/KMotionX/include/dbg.h
-
-utils.h:
-
-mongoose.h:
-
-/Users/parhansson/git/KMotionX/KMotionX/include/dbg.h:
